0bf548a342787825cb031a69727d13f8620fa4a4
Likhopoi Pavel <my@mail.com>
2020-08-26 02:28:43 +0300

    Created layout in html via bootstrapcdn.com

---
+++ index_01.php
@@ -0,0 +1,16 @@
+<?php
+
+echo rtrim();
+
+$data = [
+	'title' => 'hello world',
+	'content' => 'asdasdasd 10'
+]
+
+"SELECT * FROM tasks";
+"INSERT INTO tasks (title, content) VALUES ('asd', 'asdasd')";
+"UPDATE tasks SET title='asdasd' WHERE id=5";
+"DELETE FROM tasks WHERE id=5";
+
+
+?>
