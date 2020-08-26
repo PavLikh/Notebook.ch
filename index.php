4cf1972f8406d2fc7d878eea8954161661f8a745
Likhopoi Pavel <my@mail.com>
2020-08-26 17:52:19 +0300

    renamed:    index.html -> index.php added foreach

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
+<?php
+
+$tasks = [
+	[
+		"id" => 15,
+		"title" => "Go to the store",
+		"content" => "asdasdasd"
+
+	],
+
+];
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-	<title>Document</title>
+	<title>Notebook</title>
 </head>
 <body>
 	<div class="container">
@@ -15,24 +29,26 @@
 				<table class="table">
 					<thead>
 						<tr>
-							<th>ID</th>
-							<th>Title</th>
+							<th><?=$task['id'];?></th>
+							<th><?=$task['title'];?></th>
 							<th>Actions</th>
 						</tr>
 					</thead>
 					<tbody>
-						<tr>
-							<td>1</td>
-							<td>Go to the store</td>
-							<td>
-								<a href="#" class="btn btn-warning">
-									Edit
-								</a>
-								<a href="#" class="btn btn-danger">
-									Delete
-								</a>
-							</td>
-						</tr>
+						<?php foreach($tasks as $task):?>
+							<tr>
+								<td>1</td>
+								<td>Go to the store</td>
+								<td>
+									<a href="#" class="btn btn-warning">
+										Edit
+									</a>
+									<a href="#" class="btn btn-danger">
+										Delete
+									</a>
+								</td>
+							</tr>	
+						<?php endforeach;?>
 					</tbody>
 				</table>
 
